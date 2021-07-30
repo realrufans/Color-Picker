eff798a471f549f017a38f3fd104c1c33f01af07
solomon stephen <33923021+realrufans@users.noreply.github.com>
2021-07-30 21:06:49 +0000

    now displaying rgb value for all colors

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ let colorCount = 10;
 
 function displayColors(colors) {
     const colorHtml = colors.map(color => {
-        return `<div class="myColor" style="background-color:${color.value}">${color.value}</div>`
+        const rgbvalue = hexToRgb(color.value)
+
+
+        return `<div class="myColor" style="background-color:${color.value}">Hex: ${color.value} </br> Rgb: (${rgbvalue.r}, ${rgbvalue.g},${rgbvalue.b})</div>`
+
     }).join('')
     myColors.innerHTML = colorHtml
 }
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
 
 
 
@@ -15,6 +30,7 @@ async function getColors(colorCount) {
     const response = await fetch(`https://apis.scrimba.com/hexcolors/?count=${colorCount}`)
     const data = await response.json()
     const colors = data.colors
+    console.log(colors)
 
     displayColors(colors)
 }
